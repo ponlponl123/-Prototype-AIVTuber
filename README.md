51dbe60ad814f3eb7bd7347b8b381f0efd594ff6
AR0N@ <55607166+ponlponl123@users.noreply.github.com>
2023-03-29 13:02:31 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 ![Pattarapong.Dev](https://github.com/ponlponl123/AIVTuber/blob/main/img/PattarapongDev.png)
-![InsanityLabs](https://github.com/InsanityLabs/AIVTuber/blob/main/img/Insanity%20Labs.png)
 
 *Translation Engine: [Google Translate](https://translate.google.com/)*
 
 **Project Check List**
 - [x]  This project is also compatible with NodeJS v16.17.1.
-
-- [x]  Better TTS.
-- [x]  OpenAI ChatGPT.
 - [ ]  This project is outdated.
 
-# AI VTuber by DevPattarapong & InsanityLabs 
+# AI VTuber by DevPattarapong
 this project is in development...
 
 * **Character Model by Live2D**
@@ -20,8 +16,22 @@ this project is in development...
 ![AI VTuber by DevPattarapong](https://github.com/ponlponl123/AIVTuber/blob/main/img/Screenshot.png)
 
 # Installation
-**See Install Doc**
-[Install Document](https://github.com/InsanityLabs/AIVTuber/blob/main/INSTALL.md)
+## TTS (Text-to-Speech) & OpenAI
+- First, you need to install all package by running the following command: `npm install dotenv`
+- Later you need to enter all the keys.
+    ![Setup Keys](https://github.com/ponlponl123/AIVTuber/blob/main/img/Screenshot%202023-01-23%20203518.png)
+    ### Twitch
+    - At the variable `twitch_username` put your username as well as the variable `twitch_username`. `twitch_channel` too.
+    - As for `twitch_token`, here are the [Docs](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow) for this.
+    ### FFMPEG
+    - The `ffmpeg_path` variable is required to use ffmpeg.exe from https://ffmpeg.org/download.html.
+    - example for this `c:/ffmpeg/bin/ffmpeg.exe`
+    ### OpenAI
+    - [This is the source for the variables. `openai_organization`](https://beta.openai.com/account/org-settings)
+    - [This is the source for the variables. `openai_apiKey`](https://beta.openai.com/account/api-keys)
+    ### AWS
+    - Go to the AWS Console at https://console.aws.amazon.com/ and get **Access keys** from Security credentials
+    ![AWS Access keys](https://github.com/ponlponl123/AIVTuber/blob/main/img/Screenshot%202023-01-23%20203227.png)
 
 ## VTube Studio API
 I'm using the [VTS Desktop Audio Plugin by Lua Lucky](https://lualucky.itch.io/vts-desktop-audio-plugin) to capture the sound and send it as Parameters so that the sides of the character can move.
--- TTS/main.js
+++ TTS/main.js
@@ -1,90 +1,96 @@
-
+//
+// Artificial intelligence (AI) VTuber by DevPattarapong (ponlponl123) 
+//
+// # ⚠️ If ffmpeg doesn't work, try the path to ffmpeg.exe
+//  like this: C:/code/AI_VTUBER/TTS/node_modules/ffmpeg-static/ffmpeg.exe
+//
+// This project is also compatible with NodeJS v16.17.1.
+//
 require('dotenv').config();
-const voice = require('elevenlabs-node');
+var twitch_username = 'TWITCH_USERNAME';
+var twitch_token = 'TWITCH_TOKEN';
+var twitch_channel = [ 'TWITCH_CHANNEL' ];
+// FFMPEG
+var ffmpeg_path = 'FFMPEG_PATH'; // like this: c:/ffmpeg/bin/ffmpeg.exe
+// OPENAI
+var openai_organization = 'ORGANIZATION';
+var openai_apiKey = 'API_KEY';
+// AWS
+var aws_region = 'REGION';
+var aws_accessKeyId = 'ACCESS_KEY_ID';
+var aws_secretAccessKey = 'SECRET_ACCESS_KEY';
+
+const host = 'localhost';
+const port = 8000;
 const express = require('express');
+const app = express();
 const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+app.get('/TTS', (req, res) => {
+    res.sendFile(__dirname + '/tts.html');
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+server.listen(port, () => {
+  console.log(`listening on ${host}:${port}`);
+});
+const AWS = require('aws-sdk');
 const fs = require('fs');
 const readline = require('readline');
 const tmi = require('tmi.js');
 const Speaker = require('speaker');
 const wav = require('wav');
-
-var ffmpeg_path = 'path'; // like this: c:/ffmpeg/bin/ffmpeg.exe
-var openai_apiKey = process.env.OPENAI_API_KEY;
-const elapiKey = 'API_KEY'; // Your API key from Elevenlabs
-const voiceID = '21m00Tcm4TlvDq8ikWAM'; // The ID of the voice you want to get
-const fileName = 'path/to/output.mp3'; // The name of your audio file
-var stability = 1.00 // do not mess with these values they are set to give human like speech
-var similarityBoost = 0.70
-const host = 'localhost';
-const port = 8000;
-const app = express();
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-app.get('', (req, res) => {
-    res.sendFile(__dirname + '/tts.html');
-});
-
-//this console log has been disabled as it caused issues with the text box in console. 
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-// });
-server.listen(port, () => {
-  console.log(`listening on ${host}:${port}`);
-});
-
-const { Writable } = require('stream');
 const { exec } = require('child_process');
 const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
+    organization: openai_organization,
     apiKey: openai_apiKey,
 });
-
 const openai = new OpenAIApi(configuration);
-
+const Polly = new AWS.Polly({
+    region: aws_region,
+    accessKeyId: aws_accessKeyId,
+    secretAccessKey: aws_secretAccessKey
+});
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+async function convertMp3ToWav(audioStream) {
+    try {
+      await fs.writeFile('path/to/output.mp3', audioStream, (err) => {
+        if (err) throw err;
+      });
+      await fs.unlink('path/to/output.wav', (err) => {
+        if (err) console.log('No previous file to delete');
+      });
+      await new Promise((resolve, reject) => {
+        exec(`"${ffmpeg_path}" -i "path/to/output.mp3" -acodec pcm_s16le -ac 1 -ar 16000 "path/to/output.wav"`, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
 
-
-//this is to get voice IDs
-// const voiceResponse = voice.getVoice(elapiKey, voiceID).then(res => {
-// 	console.log(res);
-// });
-
-async function convertMp3ToWav() {
-	try {
-		// Read the audio stream from file
-		const audioStream = fs.readFileSync('path/to/output.mp3');
-		// Delete any previous output file
-		await fs.promises.unlink('path/to/output.wav').catch(() => {});
-		// Convert the MP3 file to WAV
-		await new Promise((resolve, reject) => {
-			exec(`"${ffmpeg_path}" -i "path/to/output.mp3" -acodec pcm_s16le -ac 1 -ar 16000 "path/to/output.wav"`, (err) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});
-		});
-		console.log(`File saved.`);
-	} catch (err) {
-		console.error(err);
-	}
-}
-// put your twitch infomation here
 const client = new tmi.Client({
     options: { debug: true },
     connection: { reconnect: true },
     identity: {
-        username: 'bot_name',
-        password: 'bot_token'
+        username: twitch_username,
+        password: twitch_token
     },
-    channels: [ 'channel_name' ]
+    channels: twitch_channel
 });
 client.connect().catch((error) => {
     console.error(error);
@@ -92,12 +98,12 @@ client.connect().catch((error) => {
 var ready = true;
 async function getGptResponse(VARmessage) {
     try {
-      const gptSend = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            {role: "system", content: "system message basicly a description of the robot"},
-            {role: "user", content: `${VARmessage}`}
-        ],
+      const gptSend = await openai.createCompletion({
+        model: 'text-curie-001',
+        prompt: `${VARmessage}`,
+        temperature: 0.9,
+        max_tokens: 100,
+        stop: ["ChatGPT:", "Adrian Twarog:"],
       });
       return gptSend;
     } catch (err) {
@@ -105,141 +111,126 @@ async function getGptResponse(VARmessage) {
     }
   }
   
-  client.on('message', async (channel, tags, message, self) => {
-	if (self) return;
-	if (message) {
-		if (ready == true) {
-			ready = false;
-			const gptResponse = await getGptResponse(message);
-			console.log(`${message}`);
-			console.log(`${gptResponse.data.choices[0].message.content}`);
-			const text = gptResponse.data.choices[0].message.content;
-			voice.textToSpeechStream(elapiKey, voiceID, text, stability, similarityBoost).then(async res => {
-				// console.log('res:', res);
-				res.pipe(fs.createWriteStream(fileName));
-				res.on('error', err => {
-					console.error(err);
-				});
-				res.on('end', async () => {
-					// Save the audio stream to a file
-					await convertMp3ToWav();
-					console.log(`File saved.`);
-					import ('music-metadata').then(mm => {
-						mm.parseFile('path/to/output.mp3', {
-							native: true
-						}).then(metadata => console.log(metadata.format.duration))
-					});
-					// Play the audio
-					try {
-						const reader = new wav.Reader();
-						const speaker = new Speaker();
-						reader.pipe(speaker);
-						fs.createReadStream('path/to/output.wav').pipe(reader)
-						io.emit('TTS', {
-							subtitle: `${message}\n${gptResponse.data.choices[0].message.content}`
-						});
-						console.log('text !== "exit" : Method')
-						if (text !== 'exit') {
-							import ('music-metadata').then(mm => {
-								mm.parseFile('path/to/output.wav', {
-									native: true
-								}).then(metadata => {
-									console.log(metadata.format.duration * 1000);
-									setTimeout(() => {
-										//mic.stopRecording();
-										setTimeout(() => {
-											promptUser();
-											ready = true;
-											io.emit('TTS', {
-												subtitle: ""
-											});
-										}, 2500);
-									}, metadata.format.duration * 1000);
-								})
-								//.catch(err => console.error(err.message));
-							});
-						}
-					} catch (err) {
-						console.log(err)
-						process.exit()
-					}
-				});
-			}).catch(err => {
-				console.error(err);
-			});
-		}
-	}
-});
-
-
-
+client.on('message', async (channel, tags, message, self) => {
+    if(self) return;
+    if(message){
+        if(ready == true){
+            ready = false;
+            const gptResponse = await getGptResponse(message);            
+            const params = {
+                OutputFormat: 'mp3',
+                Text: '<speak><prosody pitch="high">'+message+".................... "+gptResponse.data.choices[0].text+'</prosody></speak>',
+                TextType: 'ssml',
+                VoiceId: 'Ivy' // Japanese voice
+            };
+            console.log(`${message}`);
+            console.log(`${gptResponse.data.choices[0].text}`);
+            Polly.synthesizeSpeech(params, async (err, data) => {
+                if (err) {
+                    console.log(err.code)
+                } else if (data) {
+                    if (data.AudioStream instanceof Buffer) {
+                        await convertMp3ToWav(data.AudioStream)
+                        console.log(`File saved.`);
+                        import('music-metadata').then( mm => {
+                            mm.parseFile('path/to/output.mp3', {native: true})
+                            .then(metadata => console.log(metadata.format.duration))
+                            .catch(err => console.error(err.message));
+                        });
+                        try{
+                            const reader = new wav.Reader();
+                            const speaker = new Speaker();
+                            reader.pipe(speaker);
+                            fs.createReadStream('path/to/output.wav').pipe(reader)
+                            io.emit('TTS', { subtitle: `${message}\n${gptResponse.data.choices[0].text}` });
+                            console.log('text !== "exit" : Method')
+                            if (ready == false) {
+                                import('music-metadata').then( mm => {
+                                    mm.parseFile('path/to/output.wav', {native: true})
+                                    .then(metadata => {
+                                        console.log(metadata.format.duration * 1000);
+                                        setTimeout(() => {
+                                            //mic.stopRecording();
+                                            setTimeout(() => {ready = true;io.emit('TTS', { subtitle: "" });}, 2500);
+                                        }, metadata.format.duration * 1000);
+                                    })
+                                    .catch(err => console.error(err.message));
+                                });
+                            }
+                        } catch (err){
+                            console.log(err)
+                            process.exit()
+                        }
+                        
+                    }
+                }
+            });
+        }
+    }
+})
 function promptUser() {
-	rl.question('Pronounce by yourself: ', async (text) => {
-		if (ready == true) {
-			ready = false;
-			console.log(`${text}`);
-			voice.textToSpeechStream(elapiKey, voiceID, text, stability, similarityBoost).then(async res => {
-				// console.log('res:', res);
-				res.pipe(fs.createWriteStream(fileName));
-				res.on('error', err => {
-					console.error(err);
-				});
-				res.on('end', async () => {
-					// Save the audio stream to a file
-					await convertMp3ToWav();
-					console.log(`File saved.`);
-					import ('music-metadata').then(mm => {
-						mm.parseFile('path/to/output.mp3', {
-							native: true
-						}).then(metadata => console.log(metadata.format.duration))
-					});
-					// Play the audio
-					try {
-						const reader = new wav.Reader();
-						const speaker = new Speaker();
-						reader.pipe(speaker);
-						fs.createReadStream('path/to/output.wav').pipe(reader)
-						io.emit('TTS', {
-							subtitle: `${text}`
-						});
-						console.log('text !== "exit" : Method')
-						if (text !== 'exit') {
-							import ('music-metadata').then(mm => {
-								mm.parseFile('path/to/output.wav', {
-									native: true
-								}).then(metadata => {
-									console.log(metadata.format.duration * 1000);
-									setTimeout(() => {
-										setTimeout(() => {
-											promptUser();
-											ready = true;
-											io.emit('TTS', {
-												subtitle: ""
-											});
-										}, 2500);
-									}, metadata.format.duration * 1000);
-								})
-							});
-						}
-					} catch (err) {
-						console.log(err)
-						process.exit()
-					}
-				});
-			}).catch(err => {
-				console.error(err);
-			});
-			if (text !== 'exit') {
-				console.log('cannot get duration from output file');
-				setTimeout(() => {
-					promptUser();
-				}, 5000);
-			} else {
-				rl.close();
-			}
-		}
-	});
+    rl.question('Pronounce by yourself: ', async (text) => {
+        if(ready == true){
+            ready = false;
+            const params = {
+                OutputFormat: 'mp3',
+                Text: '<speak><prosody pitch="high">'+text+'</prosody></speak>',
+                TextType: 'ssml',
+                VoiceId: 'Ivy' // Japanese voice
+            };
+            console.log(`${text}`);
+
+            Polly.synthesizeSpeech(params, async (err, data) => {
+                if (err) {
+                    console.log(err.code)
+                } else if (data) {
+                    if (data.AudioStream instanceof Buffer) {
+                        // Save the audio stream to a file
+                        await convertMp3ToWav(data.AudioStream)
+                        console.log(`File saved.`);
+                        import('music-metadata').then( mm => {
+                            mm.parseFile('path/to/output.mp3', {native: true})
+                            .then(metadata => console.log(metadata.format.duration))
+                            .catch(err => console.error(err.message));
+                        });
+                        // Play the audio
+                        try{
+                            const reader = new wav.Reader();
+                            const speaker = new Speaker();
+                            reader.pipe(speaker);
+                            fs.createReadStream('path/to/output.wav').pipe(reader)
+                            io.emit('TTS', { subtitle: `${text}` });
+                            console.log('text !== "exit" : Method')
+                            if (text !== 'exit') {
+                                import('music-metadata').then( mm => {
+                                    mm.parseFile('path/to/output.wav', {native: true})
+                                    .then(metadata => {
+                                        console.log(metadata.format.duration * 1000);
+                                        setTimeout(() => {
+                                            //mic.stopRecording();
+                                            setTimeout(() => {promptUser();ready = true;io.emit('TTS', { subtitle: "" });}, 2500);
+                                        }, metadata.format.duration * 1000);
+                                    })
+                                    .catch(err => console.error(err.message));
+                                });
+                            }
+                        } catch (err){
+                            console.log(err)
+                            process.exit()
+                        }
+                        
+                    }
+                }
+            });
+            if (text !== 'exit') {
+                //console.log('cannot get duration from output file');
+                // setTimeout(() => {
+                //     promptUser();
+                // }, 5000);
+            } else {
+                rl.close();
+            }
+        }
+    });
 }
-setTimeout(() => {
-	promptUser();
-}, 2500);
+setTimeout(() => { promptUser(); },2500);
